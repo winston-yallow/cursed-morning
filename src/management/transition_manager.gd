fce14eada351dbc63c1e03a322ff3c75c78ee385
Winston Yallow <44872771+winston-yallow@users.noreply.github.com>
2023-02-15 22:44:20 +0100

    Added snoozing

--- src/management/transition_manager.gd
+++ src/management/transition_manager.gd
@@ -3,11 +3,19 @@ extends Control
 
 const DreamViewportScene := preload("dream_viewport.tscn")
 const DreamViewport := preload("dream_viewport.gd")
+const Snooze := preload("res://src/quicktimes/snooze.gd")
+
+const EIGHT_HOURS := 60.0 * 60.0 * 8.0
 
 var viewports: Array[DreamViewport] = []
 var active_on_a := true
 
 @onready var canvas_layer: CanvasLayer = %CanvasLayer
+@onready var snooze: Snooze = %Snooze
+@onready var clock: Label = %Clock
+@onready var seconds: Label = %Seconds
+
+var _time := 0.0
 
 
 func _ready() -> void:
@@ -20,6 +28,15 @@ func _ready() -> void:
 	_create_vp(dream)
 	
 	EventBus.transition.connect(_transition)
+	EventBus.quicktime_failed.connect(snooze.start)
+
+
+func _process(delta: float) -> void:
+	_time += delta
+	@warning_ignore("narrowing_conversion")
+	var t := Time.get_time_dict_from_unix_time(EIGHT_HOURS + _time)
+	clock.text = "%02d:%02d" % [t.hour, t.minute]
+	seconds.text = ":%02d" % [t.second]
 
 
 func _transition(dream: Landscape) -> void:
---
+++ src/quicktimes/snooze.gd
@@ -0,0 +1,56 @@
+extends Control
+
+
+@export var increase_step := 0.25
+@export var decrease_speed := 0.35
+@export var time := 1.75
+
+@onready var anim: AnimationPlayer = %AnimationPlayer
+@onready var shader: ShaderMaterial = %Label.material
+@onready var time_label: ProgressBar = %TimeLeft
+
+var _progress := 0.0
+var _time_left := 0.0
+
+
+func _ready() -> void:
+	anim.play("hide")
+	anim.animation_set_next("show", "pulse")
+	set_process(false)
+
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("snooze") and is_processing():
+		_progress += increase_step
+
+
+func _process(delta: float) -> void:
+	_time_left -= delta
+	_progress = max(0.0, _progress - decrease_speed * delta)
+	if _progress >= 1.0:
+		finish_success()
+	if _time_left <= 0.0:
+		finish_fail()
+	time_label.value = _time_left / time
+	shader.set_shader_parameter("progress", _progress)
+
+
+func start() -> void:
+	_progress = 0.0
+	_time_left = time
+	shader.set_shader_parameter("progress", 0.0)
+	time_label.value = 0.0
+	anim.play("show")
+	set_process(true)
+
+
+func finish_success() -> void:
+	_progress = 2.0
+	anim.play("success")
+	set_process(false)
+
+
+func finish_fail() -> void:
+	_progress = 2.0
+	anim.play("fail")
+	set_process(false)
