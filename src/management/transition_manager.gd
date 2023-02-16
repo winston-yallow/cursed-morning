73250e9003d91682388e67eef84680d6918aa46d
Winston Yallow <44872771+winston-yallow@users.noreply.github.com>
2023-02-16 01:04:52 +0100

    Add snooze visualizers

--- src/management/transition_manager.gd
+++ src/management/transition_manager.gd
@@ -6,16 +6,19 @@ const DreamViewport := preload("dream_viewport.gd")
 const Snooze := preload("res://src/quicktimes/snooze.gd")
 
 const EIGHT_HOURS := 60.0 * 60.0 * 8.0
-
-var viewports: Array[DreamViewport] = []
-var active_on_a := true
+const SNOOZE_USED_COLOR := Color.DIM_GRAY
 
 @onready var canvas_layer: CanvasLayer = %CanvasLayer
 @onready var snooze: Snooze = %Snooze
 @onready var clock: Label = %Clock
 @onready var seconds: Label = %Seconds
+@onready var snooze_label_1: Label = %SnoozeLabel1
+@onready var snooze_label_2: Label = %SnoozeLabel2
+@onready var snooze_label_3: Label = %SnoozeLabel3
 
 var _time := 0.0
+var _lives := 3
+var _viewports: Array[DreamViewport] = []
 
 
 func _ready() -> void:
@@ -28,7 +31,7 @@ func _ready() -> void:
 	_create_vp(dream)
 	
 	EventBus.transition.connect(_transition)
-	EventBus.quicktime_failed.connect(snooze.start)
+	EventBus.quicktime_failed.connect(_on_quicktime_failed)
 
 
 func _process(delta: float) -> void:
@@ -39,15 +42,34 @@ func _process(delta: float) -> void:
 	seconds.text = ":%02d" % [t.second]
 
 
+func _on_quicktime_failed() -> void:
+	var snooze_label: Label
+	match _lives:
+		3: snooze_label = snooze_label_3
+		2: snooze_label = snooze_label_2
+		1: snooze_label = snooze_label_1
+	if is_instance_valid(snooze_label):
+		var t := create_tween()
+		t.tween_method(
+			func set_snooze_color(color: Color) -> void:
+				snooze_label.add_theme_color_override("font_color", color),
+			Color.WHITE,
+			SNOOZE_USED_COLOR,
+			snooze.time
+		)
+	_lives -= 1
+	snooze.start()
+
+
 func _transition(dream: Landscape) -> void:
 	_create_vp(dream)
 
 
 func _create_vp(dream: Landscape) -> void:
-	if viewports.size() > 1:
-		var old: DreamViewport = viewports.pop_front()
+	if _viewports.size() > 1:
+		var old: DreamViewport = _viewports.pop_front()
 		old.queue_free()
 	var new: DreamViewport = DreamViewportScene.instantiate()
 	add_child(new)
 	new.set_dream(dream)
-	viewports.append(new)
+	_viewports.append(new)
