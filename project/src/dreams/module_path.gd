9be69e256f4bcec80fbbd7a9a5a26f40266b029f
Winston Yallow <44872771+winston-yallow@users.noreply.github.com>
2023-02-18 23:18:06 +0100

    Fix bug with paths off-centered in module

--- project/src/dreams/module_path.gd
+++ project/src/dreams/module_path.gd
@@ -9,7 +9,8 @@ const CAMERA_TARGET := Vector3.UP * 1.3
 @export_node_path("Module") var next_module: NodePath
 
 @export_node_path("Path3D") var path_node: NodePath = "Path3D"
-@onready var _curve: Curve3D = get_node(path_node).curve
+@onready var _path: Path3D = get_node(path_node)
+@onready var _curve: Curve3D = _path.curve
 
 
 func _ready() -> void:
@@ -28,12 +29,12 @@ func _process(delta: float) -> void:
 	
 	# Calculate new positions
 	var capped_offset: float = min(offset, _curve.get_baked_length())
-	var before := to_global(_curve.sample_baked(capped_offset - 0.1))
-	var after := to_global(_curve.sample_baked(capped_offset + 0.1))
+	var before := _path.to_global(_curve.sample_baked(capped_offset - 0.1))
+	var after := _path.to_global(_curve.sample_baked(capped_offset + 0.1))
 	var dir := before.direction_to(after)
 	var dir_2d := Vector2(dir.x, dir.z)
 	character.rotation.y = -dir_2d.angle() + TAU * 0.25
-	character.position = to_global(_curve.sample_baked(offset))
+	character.position = _path.to_global(_curve.sample_baked(offset))
 	# TODO: Paths for camera positon too?
 	# Calculate camera positons relative to character
 	camera_position.position = character.to_global(CAMERA_POSITION)
@@ -49,6 +50,7 @@ func _process(delta: float) -> void:
 
 
 func get_end_point() -> Vector3:
-	# Not using _curve since we want this method to work before _ready
-	var curve: Curve3D = get_node(path_node).curve
-	return curve.get_baked_points()[curve.get_baked_points().size() - 1]
+	# Not using _path since we want this method to work before _ready
+	var path: Path3D = get_node(path_node)
+	var curve: Curve3D = path.curve
+	return path.transform * curve.get_baked_points()[curve.get_baked_points().size() - 1]
