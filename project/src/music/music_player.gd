bcd886820226109893c3a71c8320007a57fe6fb1
Winston Yallow <44872771+winston-yallow@users.noreply.github.com>
2023-02-19 13:45:45 +0100

    Fix music playback and reduce volume

--- project/src/music/music_player.gd
+++ project/src/music/music_player.gd
@@ -1,12 +1,31 @@
 extends AudioStreamPlayer
 
 
-const VOLUME_LOW := -15.0
-const VOLUME_HIGH := -5.0
+const VOLUME_LOW := -30.0
+const VOLUME_HIGH := -15.0
+
+const options: Array[AudioStream] = [
+	preload("res://src/music/devotion.mp3"),
+	preload("res://src/music/please_stay.mp3"),
+	preload("res://src/music/trapped.mp3")
+]
+
+var _last: AudioStream
 
 
 func _ready() -> void:
 	set_volume_lerp(1.0)
+	finished.connect(_play_random)
+	_play_random()
+
+
+func _play_random() -> void:
+	var possible := options.duplicate()
+	if _last in possible:
+		possible.erase(_last)
+	stream = possible.pick_random()
+	_last = stream
+	play()
 
 
 func set_volume_lerp(loudness: float) -> void:
